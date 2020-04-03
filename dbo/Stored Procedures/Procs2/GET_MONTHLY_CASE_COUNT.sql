1e7659bb1a25a9c188ae425a824e26f2edf8bcd2
deepakv <deepak.v@codearray.tech>
2020-04-03 14:29:37 +0530

    Get Monthly Case Count SP added for automation

---
+++ dbo/Stored Procedures/Procs2/GET_MONTHLY_CASE_COUNT.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[GET_MONTHLY_CASE_COUNT]
+AS
+BEGIN
+	SELECT UPPER(DomainId) [DomainId]
+		,COUNT(Case_Id) [Count]
+		,DATENAME(MONTH, Date_Opened) [CaseOpenedMonth]
+		,YEAR(Date_Opened) [CaseOpenedYear]
+	FROM tblCase
+	WHERE MONTH(Date_Opened) = MONTH(GETDATE() - 1)
+		AND YEAR(Date_Opened) = YEAR(GETDATE())
+		AND isnull(IsDeleted, 0) = 0
+		AND UPPER(DomainId) <> 'TEST'
+	GROUP BY DomainId
+		,DATENAME(MONTH, Date_Opened)
+		,YEAR(Date_Opened)
+	ORDER BY DomainID
+END
