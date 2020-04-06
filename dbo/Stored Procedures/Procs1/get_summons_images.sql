5c6fb2c1aba32051f6696a372c680a9e8312f59b
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-06 22:53:53 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/get_summons_images.sql
+++ dbo/Stored Procedures/Procs1/get_summons_images.sql
@@ -6,7 +6,12 @@ as
 BEGIN 
 CREATE TABLE #TEMP(Image_Id int, Filename NVARCHAR(500))
 
-IF EXISTS(select * from TBLDOCIMAGES I Join tblImageTag IT on IT.ImageID=i.ImageID Join tblTags T on T.NodeID = IT.TagID and T.CaseID= @CaseId and NodeName in ('A.O.B') and Filename like '%.pdf%')
+IF EXISTS(select * from TBLDOCIMAGES I Join tblImageTag IT on IT.ImageID=i.ImageID Join tblTags T on T.NodeID = IT.TagID and T.CaseID= @CaseId and NodeName in ('A.O.B') and Filename like '%.pdf%'
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+WHERE I.IsDeleted=0 AND IT.IsDeleted=0
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+
+)
 BEGIN
 	INSERT INTO #TEMP
 	SELECT 0,'exhibits\Summons\A Exhibit.pdf'
@@ -17,9 +22,17 @@ BEGIN
 	  TBLDOCIMAGES I
 	  Join tblImageTag IT on IT.ImageID=i.ImageID
 	  Join tblTags T on T.NodeID = IT.TagID and T.CaseID= @CaseId and NodeName in ('A.O.B') and Filename like '%.pdf%'
+	    ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+	  WHERE I.IsDeleted=0 AND IT.IsDeleted=0	
+      ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+    
 END
 
-IF EXISTS(select * from TBLDOCIMAGES I Join tblImageTag IT on IT.ImageID=i.ImageID Join tblTags T on T.NodeID = IT.TagID and T.CaseID= @CaseId and NodeName in ('Bills') and Filename like '%.pdf%')
+IF EXISTS(select * from TBLDOCIMAGES I Join tblImageTag IT on IT.ImageID=i.ImageID Join tblTags T on T.NodeID = IT.TagID and T.CaseID= @CaseId and NodeName in ('Bills') and Filename like '%.pdf%'
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+	WHERE I.IsDeleted=0 AND IT.IsDeleted=0
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+)
 BEGIN
 	INSERT INTO #TEMP
 	SELECT 0,'exhibits\Summons\B Exhibit.pdf'
@@ -30,9 +43,16 @@ BEGIN
 	  TBLDOCIMAGES I
 	  Join tblImageTag IT on IT.ImageID=i.ImageID
 	  Join tblTags T on T.NodeID = IT.TagID and T.CaseID= @CaseId and NodeName in ('Bills') and Filename like '%.pdf%'
+	  ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+	WHERE I.IsDeleted=0 AND IT.IsDeleted=0
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
 END
   
-IF NOT EXISTS(select * from TBLDOCIMAGES I Join tblImageTag IT on IT.ImageID=i.ImageID Join tblTags T on T.NodeID = IT.TagID and T.CaseID= @CaseId and NodeName in ('A.O.B') and Filename like '%.pdf%')
+IF NOT EXISTS(select * from TBLDOCIMAGES I Join tblImageTag IT on IT.ImageID=i.ImageID Join tblTags T on T.NodeID = IT.TagID and T.CaseID= @CaseId and NodeName in ('A.O.B') and Filename like '%.pdf%'
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+	WHERE I.IsDeleted=0 AND IT.IsDeleted=0
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+)
 BEGIN
   UPDATE #TEMP SET Filename='exhibits\Summons\A Exhibit.pdf' WHERE Image_Id =0
 END
