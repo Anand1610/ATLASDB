107943b4e7ca8da07bfe29d67aa81f117f7c31fa
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-06 18:34:35 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/SpGetCasesFiles.sql
+++ dbo/Stored Procedures/Procs2/SpGetCasesFiles.sql
@@ -19,5 +19,9 @@ BEGIN
 	where		doc.DomainId = @DOMAINID
 	and			tag.CaseID = @CASEID
 	and			tag.NodeName = @NODENAME
+	 ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+  AND doc.IsDeleted=0 AND imgtag.IsDeleted=0   
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+  
 END
 
