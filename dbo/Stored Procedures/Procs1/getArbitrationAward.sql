14f00cb2b499b9db9d4575453bd45e82faa151f6
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-06 18:26:51 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/getArbitrationAward.sql
+++ dbo/Stored Procedures/Procs1/getArbitrationAward.sql
@@ -16,6 +16,10 @@ inner join dbo.tbltransactions
 on tblTags.CaseID =tbltransactions.case_id 
 where provider_id = @pid and invoice_id = @iid and transactions_type = 'C'
 and NodeName ='Arbitration Awards'
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+    
+AND tblDocImages.IsDeleted=0 AND tblImageTag.IsDeleted=0  
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
 
 
 end
