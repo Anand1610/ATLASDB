e07b4c56be319a85240d80e587ab669665773500
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-06 23:42:37 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/F_Temp_DM_FileMoved.sql
+++ dbo/Stored Procedures/Procs1/F_Temp_DM_FileMoved.sql
@@ -34,6 +34,9 @@ BEGIN
 	AND I.BasePathId = 2
 	-- AND statusdone ='File Not Found'
 	AND statusdone IS NULL
+	---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+	AND I.IsDeleted=0 AND  IT.IsDeleted=0 
+    ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
  order by I.ImageID  
       
 END  
