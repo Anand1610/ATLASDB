50075fab1d91884abdbd2458575ada00cee3519c
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-06 23:16:24 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/STP_GETIMAGEDATA.sql
+++ dbo/Stored Procedures/Procs2/STP_GETIMAGEDATA.sql
@@ -4,4 +4,8 @@
 AS
 
 SELECT * FROM tblDocImages WHERE IMAGEID=@IMAGEID
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+AND IsDeleted=0		
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+
 
