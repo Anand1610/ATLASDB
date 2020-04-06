c3a30804e0c6ab4b55039ad035df1e51eba667c9
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-06 16:59:50 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/GetMedicalReports.sql
+++ dbo/Stored Procedures/Procs1/GetMedicalReports.sql
@@ -18,6 +18,9 @@ left JOIN tblDocImages docimg with(nolock)  on tblImg.ImageID = docimg.ImageID
 where Provider_Id IN (SELECT  cast(items as INT )  FROM dbo.STRING_SPLIT(@Provider_Id,',')) and  
 InsuranceCompany_Id IN (SELECT  cast(items as INT )  FROM dbo.STRING_SPLIT(@InsuranceCompany_Id,','))
 and tgs.nodename=@nodename and  Filename not like'%mr.pdf%'
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		AND tblImg.IsDeleted =0 AND docimg.IsDeleted=0
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
 order by t1.Case_Id
 
 end 
