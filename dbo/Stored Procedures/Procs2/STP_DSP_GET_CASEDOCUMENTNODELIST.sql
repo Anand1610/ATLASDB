90ab30ac4f8e73c033132f2cdd3e9e53b4406b6a
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-06 17:40:33 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/STP_DSP_GET_CASEDOCUMENTNODELIST.sql
+++ dbo/Stored Procedures/Procs2/STP_DSP_GET_CASEDOCUMENTNODELIST.sql
@@ -15,7 +15,13 @@ IF @ISARCHIVED='0'
 	T.NodeLevel+1, 1 from TBLDOCIMAGES I  
 	Inner Join tblImageTag IT on IT.ImageID=i.ImageID  
 	Inner Join tblTags T on T.NodeID=IT.TagID and T.CaseID=@CASEID 
-	left join tblApplicationSettings s on s.parametername='DocumentUploadLocation' order by nodelevel, nodename
+	left join tblApplicationSettings s on s.parametername='DocumentUploadLocation' 
+	
+	---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		WHERE I.IsDeleted=0 AND IT.IsDeleted=0
+   ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+
+	order by nodelevel, nodename
 ELSE
 	Select cast(T.NodeID as nvarchar) NodeID, T.ParentID, T.NodeName, T.NodeIcon, T.NodeLevel, T.Expanded 
 	from tblTags T Where CaseID=@CASEID  
@@ -26,5 +32,9 @@ ELSE
 	T.NodeLevel+1, 1 from TBLDOCIMAGES I  
 	Inner Join tblImageTag IT on IT.ImageID=i.ImageID  
 	Inner Join tblTags T on T.NodeID=IT.TagID and T.CaseID=@CASEID 
-	left join tblApplicationSettings s on s.parametername='ArchivedDocumentUploadLocation' order by nodelevel, nodename
+	left join tblApplicationSettings s on s.parametername='ArchivedDocumentUploadLocation' 
+	---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		WHERE I.IsDeleted=0 AND IT.IsDeleted=0
+   ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+	order by nodelevel, nodename
 
