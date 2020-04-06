9ca46b803423cca907bc8b96843bf23c040ba8cf
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-06 17:03:09 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/GetNodeDocuments.sql
+++ dbo/Stored Procedures/Procs1/GetNodeDocuments.sql
@@ -17,6 +17,12 @@ from tblTags tgs with(nolock)
 
 where tgs.nodename in ('Correspondence' ,'Defense pleadings','DISCOVERY','MOTIONS/RESPONSES','Plaintiff pleadings','Proof of Notice','Checks and Releases','Settlement Letter')
 and tgs.caseid=@CASEID and tgs.DomainId=@DOMAINID 
+
+
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+	AND tblImg.IsDeleted=0 AND docimg.IsDeleted=0 	
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+
 end
 else
 begin
@@ -29,6 +35,9 @@ from tblTags tgs with(nolock)
 
 where tgs.nodename =@NODENAME
 and tgs.caseid=@CASEID and tgs.DomainId=@DOMAINID and tblImg.DomainId=@DOMAINID and docimg.DomainId=@DOMAINID
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+AND tblImg.IsDeleted=0 AND docimg.IsDeleted=0 	
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
 end
 end
 
