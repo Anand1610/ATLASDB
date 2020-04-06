b1b16a48a8afd16d8d43090a348d7f9238439bf3
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-06 12:39:06 +0530

    IsDeleted=0 added

--- dbo/Stored Procedures/Procs1/batch_print_offline_fax_queue_retrieve.sql
+++ dbo/Stored Procedures/Procs1/batch_print_offline_fax_queue_retrieve.sql
@@ -26,6 +26,9 @@ BEGIN
 		ISNULL(Q.isDeleted,0)		=	0	AND
 		ISNULL(R.isDeleted,0)		=	0	AND
 		ISNULL(IsAddedtoQueue,0)	=	0
+	   ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		AND FI.IsDeleted=0
+       --- End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
 	ORDER BY
 		pk_bp_fax_queue_id ASC
 END
