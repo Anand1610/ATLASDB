a045e8ae3e5ca42509634248f07f18a619e26d91
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-06 17:09:36 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/GetPhysicalFile.sql
+++ dbo/Stored Procedures/Procs1/GetPhysicalFile.sql
@@ -7,4 +7,7 @@ select D.FilePath,D.filename,D.BasePathId
 from tblDocImages D (nolock) 
 Left Join tblBasePath B ON B.BasePathId=D.BasePathId 
 where D.imageid =  @imageid and D.DomainId= @DomainId
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+	AND D.IsDeleted=0	
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
 end
