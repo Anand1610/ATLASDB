238c64e24fb6eb9afd049b7e57a774d19a84115c
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-06 19:03:18 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/F_Get_ARBFILED.sql
+++ dbo/Stored Procedures/Procs1/F_Get_ARBFILED.sql
@@ -16,6 +16,10 @@ AS
 			INNER JOIN dbo.LCJ_VW_CaseSearchDetails LCJ ON T.CaseID=LCJ.Case_Id
 			Where FilePath like ''%ARB Docs%'' and Filename like ''%_ARB_FILED_%''  and Filename like ''%.pdf''
 			and LCJ.INSURANCE_NAME not in ( ''ALLSTATE INSURANCE COMPANY'',''GEICO INSURANCE COMPANY'') AND IT.IsDeleted=0 and I.IsDeleted=0 '
+		---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+             -- and IT.IsDeleted=0 and I.IsDeleted=0   added
+            ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude 
+		
 		END  
 		ELSE
 		IF (@rblArb = '0')
@@ -25,7 +29,9 @@ AS
 			Inner Join tblTags T on T.NodeID=IT.TagID 
 			INNER JOIN dbo.LCJ_VW_CaseSearchDetails LCJ ON T.CaseID=LCJ.Case_Id
 			Where IT.IsDeleted=0 and I.IsDeleted=0 AND FilePath like ''%ARB Docs%'' and Filename like ''%AAA DEMAND PACKET%''  and Filename like ''%.pdf'''
-			
+			---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+             --IT.IsDeleted=0 and I.IsDeleted=0 and  added
+            ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude 
 		END
 		if @dt_From <>'1/1/0001' and @dt_To<>'1/1/0001' and @dt_From<>'' and @dt_To<>''
 		begin  
