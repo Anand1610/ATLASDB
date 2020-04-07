5de53f7a583f980d0defc6abc2e2b95819b89093
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-07 16:14:09 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/F_Get_ARBAWARD.sql
+++ dbo/Stored Procedures/Procs1/F_Get_ARBAWARD.sql
@@ -21,6 +21,9 @@ AS
             INNER JOIN dbo.tblProvider WITH (NOLOCK) ON lcj.Provider_Id = dbo.tblProvider.Provider_Id 
 			WHERE ISNULL(lcj.IsDeleted,0) = 0  AND NodeName in ('Arbitration Awards','MASTER ARBITRATION AWARD') 
 			AND lcj.Provider_Id=@pro_id AND DateInserted BETWEEN @dt_From AND @dt_To
+			---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		    and tblDocImages.IsDeleted=0 AND tblImageTag.IsDeleted=0
+            ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
  
 		END  
 		ELSE
@@ -37,6 +40,10 @@ AS
             INNER JOIN dbo.tblProvider pro WITH (NOLOCK) ON lcj.Provider_Id =pro.Provider_Id
 			INNER JOIN dbo.TblProvider_Groups pro_grp  with(nolock) ON pro_grp.Provider_Group_Name=pro.Provider_GroupName
 			WHERE  ISNULL(lcj.IsDeleted,0) = 0 AND NodeName in ('Arbitration Awards','MASTER ARBITRATION AWARD')  AND pro.Provider_GroupName=@pro_grp AND DateInserted BETWEEN @dt_From AND @dt_To
+		   ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		    and tblDocImages.IsDeleted=0 AND tblImageTag.IsDeleted=0 
+            ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		
 		END
 		IF (@pro_id = '0' and @pro_grp = '0')
 		BEGIN
@@ -50,7 +57,9 @@ AS
 			INNER JOIN  dbo.tblInsuranceCompany WITH (NOLOCK) ON lcj.InsuranceCompany_Id = dbo.tblInsuranceCompany.InsuranceCompany_Id   
             INNER JOIN dbo.tblProvider pro WITH (NOLOCK) ON lcj.Provider_Id =pro.Provider_Id
 			WHERE ISNULL(lcj.IsDeleted,0) = 0 AND NodeName in ('Arbitration Awards','MASTER ARBITRATION AWARD') AND DateInserted BETWEEN @dt_From AND @dt_To
- 
+             ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		    and tblDocImages.IsDeleted=0 AND tblImageTag.IsDeleted=0
+            ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
 		END  
 	END
 	SET NOCOUNT OFF;
