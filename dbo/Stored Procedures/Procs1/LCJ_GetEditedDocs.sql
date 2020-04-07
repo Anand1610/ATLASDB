80cdcd405f3b96de2d8a70990b9f8ad14e1111c9
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-07 23:58:12 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/LCJ_GetEditedDocs.sql
+++ dbo/Stored Procedures/Procs1/LCJ_GetEditedDocs.sql
@@ -7,6 +7,12 @@ begin
 Select TG.NodeID[Node Id],TD.Filename [File Name],TD.ImageID[Image Id] from tblTags TG 
 join tblImageTag TT on TG.NodeId=TT.TagID 
 join tblDocImages TD on TD.ImageID=TT.ImageID
-where TG.NodeType='SVDLET' and CaseID=@Case_Id Order By Td.ImageID
+where TG.NodeType='SVDLET' and CaseID=@Case_Id 
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+  AND TT.IsDeleted =0   AND TD.IsDeleted=0  
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+
+Order By Td.ImageID
+
 end
 
