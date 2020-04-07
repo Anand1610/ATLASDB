c8455f17346a8b5318acc0dd8a152582a9f07a4f
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-07 17:02:55 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/SP_GET_FILE_PATH_NEW.sql
+++ dbo/Stored Procedures/Procs2/SP_GET_FILE_PATH_NEW.sql
@@ -11,6 +11,9 @@ BEGIN
         JOIN TBLTAGS T ON T.NODEID = IT.TAGID 
         JOIN @Images IM ON I.ImageID=IM.ImageId
 		LEFT OUTER JOIN tblBasePath B ON B.BasePathId=I.BasePathId
+		  ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+    WHERE I.IsDeleted=0 AND IT.IsDeleted=0   
+  ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude 
         --LEFT JOIN TBLAPPLICATIONSETTINGS S ON S.PARAMETERNAME= 'DocumentUploadLocationPhysical'
         ORDER BY I.IMAGEID
    
