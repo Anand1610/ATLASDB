db9ff56cabd4c126a2b7eb232f0c09f7f7088edf
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-07 19:19:14 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/SP_GET_BASEPATHID_Of_File.sql
+++ dbo/Stored Procedures/Procs2/SP_GET_BASEPATHID_Of_File.sql
@@ -8,4 +8,7 @@ BEGIN
 	
 	SET NOCOUNT ON;	
     select BasePathId from tblDocImages where ImageID=@ImageId and DomainId=@DomainId
+	---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+   AND IsDeleted=0  
+    ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
 END
