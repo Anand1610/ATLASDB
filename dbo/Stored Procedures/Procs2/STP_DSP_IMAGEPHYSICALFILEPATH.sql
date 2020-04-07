6444ecb284fe26f293d1a52dd24198da09178034
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-07 23:53:56 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/STP_DSP_IMAGEPHYSICALFILEPATH.sql
+++ dbo/Stored Procedures/Procs2/STP_DSP_IMAGEPHYSICALFILEPATH.sql
@@ -21,7 +21,9 @@ BEGIN
 	----------------------For Azure File
     SELECT FILEPATH + FILENAME as [InputFilePath],I.BasePathId FROM tblDocImages I Left Join tblBasePath B ON B.BasePathId=I.BasePathId
 	WHERE IMAGEID=@IMAGEID and DomainId=@DomainId
-
+	 ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+   and IsDeleted=0  
+    ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
 	
 
 
