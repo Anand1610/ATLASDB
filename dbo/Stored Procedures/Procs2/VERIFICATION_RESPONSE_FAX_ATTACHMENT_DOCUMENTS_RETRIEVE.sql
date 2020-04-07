3a2c46bc4689ece11af6d8eaca94b5c600f876ef
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-07 16:20:34 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/VERIFICATION_RESPONSE_FAX_ATTACHMENT_DOCUMENTS_RETRIEVE.sql
+++ dbo/Stored Procedures/Procs2/VERIFICATION_RESPONSE_FAX_ATTACHMENT_DOCUMENTS_RETRIEVE.sql
@@ -21,6 +21,9 @@ begin
 	WHERE
 		FATT.i_verification_id	=	@VerificationId AND
 		FATT.IsDeleted			=	0
+		 ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+        AND FI.IsDeleted=0  
+        ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude 
 	ORDER BY
 		pk_vr_fax_attachment_id ASC
 end
