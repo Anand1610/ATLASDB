7dc35e646314a29d34c4e0a894fa5ef3618a78bb
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-08 17:04:44 +0530

    comment added

--- dbo/Stored Procedures/Procs1/Get_NoticeofTrial_Report.sql
+++ dbo/Stored Procedures/Procs1/Get_NoticeofTrial_Report.sql
@@ -283,6 +283,10 @@ ELSE ''No'' END
 
 
 
+		 ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+		 --WHERE I.IsDeleted=0 AND IT.IsDeleted=0  
+		   ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+
 
 
 
