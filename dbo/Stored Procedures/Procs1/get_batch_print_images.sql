9431cac671e077004b2044e65f5b6a41dfa35557
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-08 17:30:02 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/get_batch_print_images.sql
+++ dbo/Stored Procedures/Procs1/get_batch_print_images.sql
@@ -82,6 +82,10 @@ BEGIN
 		LTRIM(RTRIM(bod.Batch_Type))=@s_a_BatchType AND 
 		LOWER(I.Filename) like '%.pdf%'
 		AND NodeName = 'SUMMONS AND COMPLAINT'
+		
+  ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+  AND I.IsDeleted=0 AND IT.IsDeleted=0  
+        ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
 	order by
 		Sequence_No, DateInserted desc
 	-----Otehr documents except Summons and complaint---------------------------------------------------------------------------
@@ -124,6 +128,9 @@ BEGIN
 		LTRIM(RTRIM(bod.Batch_Type))=@s_a_BatchType AND 
 		LOWER(I.Filename) like '%.pdf%'
 		AND NodeName <> 'SUMMONS AND COMPLAINT'
+		  ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+        AND I.IsDeleted=0 AND IT.IsDeleted=0  
+        ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
 	order by
 	Sequence_No, DateInserted asc
 
