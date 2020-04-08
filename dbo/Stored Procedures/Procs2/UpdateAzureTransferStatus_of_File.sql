707c08ae5188df995ab64ecceac8895e564511ab
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-08 23:35:35 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/UpdateAzureTransferStatus_of_File.sql
+++ dbo/Stored Procedures/Procs2/UpdateAzureTransferStatus_of_File.sql
@@ -11,6 +11,9 @@ BEGIN
 	-- if exists(select * from tblDocImages where ImageID =@ImageId )
 	 --begin
 	 update tblDocImages set statusdone =@TransferStatus,BasePathId=@BasePathId where ImageID=@ImageId 
+	  ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+     AND IsDeleted =0  
+    ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
 	 --and DomainId='AF' 
 	-- end
 
