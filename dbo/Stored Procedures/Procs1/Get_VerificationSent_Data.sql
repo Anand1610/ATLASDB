ce5102de075c55a4980b704e87ddb768f631c2a4
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-08 17:51:18 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/Get_VerificationSent_Data.sql
+++ dbo/Stored Procedures/Procs1/Get_VerificationSent_Data.sql
@@ -40,14 +40,16 @@ BEGIN
 			   INNER JOIN tblcase cas (NOLOCK) on VR.SZ_CASE_ID = cas.Case_Id and GB_CASE_ID IS NOT NULL 
 			   INNER JOIN tblTreatment Tre (NOLOCK)  ON VR.SZ_CASE_ID=Tre.Case_Id 
 			   LEFT JOIN tblDocImages RI (NOLOCK) ON RI.ImageID = VR.ManualResponseImageID AND RI.DomainId = VR.DomainID 
+			  ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		        AND RI.IsDeleted=0
+               ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+			  
 			   LEFT JOIN tblBasePath RIBP (NOLOCK) ON RIBP.BasePathId = RI.BasePathId                         
 			WHERE                            
 				DT_VERIFICATION_REPLIED BETWEEN @FROMDate AND @ToDate+1
 				AND cas.gb_case_id is not null
 				AND RIBP.BasePathType = 2
-				---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
-		        AND RI.IsDeleted=0
-               ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+				
 
 			UNION
 
@@ -60,14 +62,15 @@ BEGIN
 				JOIN TXN_VERIFICATION_REQUEST VR (NOLOCK) ON VR.I_VERIFICATION_ID = FATT.I_VERIFICATION_ID
 				INNER JOIN tblcase cas (NOLOCK) on VR.SZ_CASE_ID = cas.Case_Id and GB_CASE_ID IS NOT NULL
 				JOIN tblDocImages FI (NOLOCK) ON FI.ImageID = FATT.FaxImageID
+				---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		        AND FI.IsDeleted=0
+                ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
 				JOIN tblBasePath FIBP (NOLOCK) ON FIBP.BasePathId = FI.BasePathId                      
 			WHERE                            
 				DT_VERIFICATION_REPLIED BETWEEN @FROMDate AND @ToDate+1
 				AND cas.gb_case_id is not null
 				AND FIBP.BasePathType=2
-				---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
-		        AND FI.IsDeleted=0
-                ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+				
 			ORDER BY i_verification_id DESC
 	END
 	ELSE IF(@Param = 'File')
@@ -82,13 +85,15 @@ BEGIN
 			   INNER JOIN tblcase cas (NOLOCK) on VR.SZ_CASE_ID = cas.Case_Id and GB_CASE_ID IS NOT NULL 
 			   INNER JOIN tblTreatment Tre (NOLOCK)  ON VR.SZ_CASE_ID=Tre.Case_Id 
 			   LEFT JOIN tblDocImages RI (NOLOCK) ON RI.ImageID = VR.ManualResponseImageID AND RI.DomainId = VR.DomainID 
+			  	---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		        AND RI.IsDeleted=0
+                ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+			  
 			   LEFT JOIN tblBasePath RIBP (NOLOCK) ON RIBP.BasePathId = RI.BasePathId                         
 			WHERE                            
 				DT_VERIFICATION_REPLIED BETWEEN @FROMDate AND @ToDate+1
 				AND cas.gb_case_id is not null
-					---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
-		        AND RI.IsDeleted=0
-                ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+				
 				--AND RIBP.BasePathType != 2
 			UNION
 
@@ -102,13 +107,14 @@ BEGIN
 				JOIN TXN_VERIFICATION_REQUEST VR (NOLOCK) ON VR.I_VERIFICATION_ID = FATT.I_VERIFICATION_ID
 				INNER JOIN tblcase cas (NOLOCK) on VR.SZ_CASE_ID = cas.Case_Id and GB_CASE_ID IS NOT NULL 
 				JOIN tblDocImages FI (NOLOCK) ON FI.ImageID = FATT.FaxImageID
+				---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+		        AND FI.IsDeleted=0
+                ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
 				JOIN tblBasePath FIBP (NOLOCK) ON FIBP.BasePathId = FI.BasePathId                      
 			WHERE                            
 				DT_VERIFICATION_REPLIED BETWEEN @FROMDate AND @ToDate+1
 				AND cas.gb_case_id is not null
-				---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
-		        AND FI.IsDeleted=0
-                ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+				
 				--AND FIBP.BasePathType!= 2
 			ORDER BY i_verification_id DESC
 	END
