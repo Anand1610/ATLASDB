5e8e213b71b8b31b027a04942d0a65677d5b8ae8
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-08 18:34:07 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/getArbitrationAward.sql
+++ dbo/Stored Procedures/Procs1/getArbitrationAward.sql
@@ -10,16 +10,16 @@ begin
 seLect * fRoM tblDocImages 
 left outer join tblImageTag 
 on tblDocImages.imageid=tblImageTag.imageid
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+AND tblDocImages.IsDeleted=0 AND tblImageTag.IsDeleted=0  
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
 left outer join tblTags 
 on tblTags.NodeID = tblImageTag.TagID
 inner join dbo.tbltransactions 
 on tblTags.CaseID =tbltransactions.case_id 
 where provider_id = @pid and invoice_id = @iid and transactions_type = 'C'
 and NodeName ='Arbitration Awards'
----Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
-    
-AND tblDocImages.IsDeleted=0 AND tblImageTag.IsDeleted=0  
----End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+
 
 
 end
