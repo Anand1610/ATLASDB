caecc9f8209bbe3917be92a6c45b32ebeb2c91e3
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-08 18:36:00 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs1/getProviderChecks.sql
+++ dbo/Stored Procedures/Procs1/getProviderChecks.sql
@@ -11,6 +11,9 @@ begin
 seLect * fRoM tblDocImages 
 left outer join tblImageTag 
 on tblDocImages.imageid=tblImageTag.imageid
+---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+	AND tblDocImages.IsDeleted=0 AND  tblImageTag.IsDeleted=0	
+---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
 left outer join tblTags 
 on tblTags.NodeID = tblImageTag.TagID
 inner join dbo.tbltransactions 
@@ -19,9 +22,7 @@ where provider_id = @pid and invoice_id = @iid and transactions_type = 'C'
 and NodeName ='PAYMENTS - PROVIDER'
 and tblDocImages.imageid not in(295161,293736)
 and tblDocImages.DomainId=@DomainId
----Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
-	AND tblDocImages.IsDeleted=0 AND  tblImageTag.IsDeleted=0	
----End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude
+
 
 end
 
