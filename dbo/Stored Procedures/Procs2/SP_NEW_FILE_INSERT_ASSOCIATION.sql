e7d0d8c1760cc4f2dffb1778043dfa6d53fcc074
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-12 23:22:08 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/SP_NEW_FILE_INSERT_ASSOCIATION.sql
+++ dbo/Stored Procedures/Procs2/SP_NEW_FILE_INSERT_ASSOCIATION.sql
@@ -33,8 +33,17 @@ set deadlock_priority 10
 		SET @i_l_max_id  = 0      
 		DECLARE @i_l_duplicate INT = 0    
 
-		 SELECT @i_l_duplicate = COUNT(*) FROM tblDocImages (NOLOCK) WHERE FilePath = @s_a_file_path AND Filename = @s_a_filename and ImageID in   
-		 (SELECT ImageID FROM tblImageTag  (NOLOCK) WHERE TagID IN (SELECT NodeID FROM tblTags (NOLOCK) WHERE  DomainId = @DomainId And ltrim(rtrim(caseid)) = ltrim(rtrim( @s_a_case_id))))
+		 SELECT @i_l_duplicate = COUNT(*) FROM tblDocImages (NOLOCK) WHERE FilePath = @s_a_file_path AND Filename = @s_a_filename and 
+		  
+       ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+       tblDocImages.IsDeleted=0 AND   
+         ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+		 ImageID in   
+		 (SELECT ImageID FROM tblImageTag  (NOLOCK) WHERE 
+		  ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+        tblImageTag.IsDeleted=0 AND  
+        ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+		 TagID IN (SELECT NodeID FROM tblTags (NOLOCK) WHERE  DomainId = @DomainId And ltrim(rtrim(caseid)) = ltrim(rtrim( @s_a_case_id))))
 
 		 IF(@i_l_duplicate = 0)      
 		 BEGIN      
