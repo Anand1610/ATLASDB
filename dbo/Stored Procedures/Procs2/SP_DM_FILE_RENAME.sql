b7e812eda04b518e48fe047eeaeafbaac2c3888a
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-12 14:08:54 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/SP_DM_FILE_RENAME.sql
+++ dbo/Stored Procedures/Procs2/SP_DM_FILE_RENAME.sql
@@ -16,6 +16,10 @@ BEGIN
 	WHERE 
 		imageid=@i_a_image_id
 	AND	DomainId=@DomainId
+	 ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+    AND IsDeleted =0  
+    ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+  
 		
 	UPDATE 
 		tblImageTag 
@@ -24,6 +28,10 @@ BEGIN
 	WHERE 
 		IMAGEID=@i_a_image_id
 	AND DomainId=@DomainId
+	 ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+    AND IsDeleted =0  
+    ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+  
 		
 	IF EXISTS(SELECT 1 FROM tbl_ImageTag_Modifiedby  WHERE imageid =@i_a_image_id and DomainId=@DomainId)
 		BEGIN
