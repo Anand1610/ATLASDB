977a9b96b89617293dc327ee7d3648646e4b7e7d
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-12 10:35:59 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/SP_SAVE_MERGED_PDF_IN_MGR.sql
+++ dbo/Stored Procedures/Procs2/SP_SAVE_MERGED_PDF_IN_MGR.sql
@@ -21,7 +21,11 @@ BEGIN
 			WHERE DomainId = @DomainId
 				AND lower([filename]) = lower(@p_szFileName)
 				AND lower(filepath) = lower(@p_szYearPath)
+						   ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+                AND IsDeleted=0  
+                ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
 			)
+	
 	SET @ParentID = (
 			SELECT MIN(nodeid)
 			FROM tbltags(NOLOCK)
