202d80eaf14b32d6f46684492b161de16e1248bf
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-04-12 23:26:49 +0530

    IsDeleted=0 condition added

--- dbo/Stored Procedures/Procs2/SP_NEW_FILE_INSERT_ASSOCIATION_WITH_RETURN_IMAGEID.sql
+++ dbo/Stored Procedures/Procs2/SP_NEW_FILE_INSERT_ASSOCIATION_WITH_RETURN_IMAGEID.sql
@@ -51,10 +51,10 @@ set deadlock_priority 10
 		 FilePath = @s_a_file_path AND Filename = @s_a_filename and ImageID in   
 		 (SELECT ImageID FROM tblImageTag  (NOLOCK) WHERE 
 		 
-		      
-   ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
-   WHERE tblImageTag.IsDeleted=0  
-       ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+		    ---Start of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude  
+    
+        tblImageTag.IsDeleted=0 AND   
+       ---End   of  changes for LSS-470 done on 5 APRIL 2020  By Tushar Chandgude 
 		 TagID IN (SELECT NodeID FROM tblTags (NOLOCK) WHERE  DomainId = @DomainId And ltrim(rtrim(caseid)) = ltrim(rtrim( @s_a_case_id))))
 
 		 IF(@i_l_duplicate = 0)      
