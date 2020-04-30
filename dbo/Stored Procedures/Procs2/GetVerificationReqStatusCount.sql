1adad9d57096c4a5c2d3a75a0fd2f1814104b059
deepakv <deepak.v@codearray.tech>
2020-04-30 18:39:50 +0530

    Abhay's new SP added

---
+++ dbo/Stored Procedures/Procs2/GetVerificationReqStatusCount.sql
@@ -0,0 +1,14 @@
+﻿/*    ==Scripting Parameters==
+
+ Created By = Abhay.W
+ Created Date = 04/30/2020
+*/
+
+CREATE PROCEDURE [dbo].[GetVerificationReqStatusCount]
+(
+	@DomainId VARCHAR(10)
+)
+AS
+BEGIN
+	SELECT count(VerificationStatus) AS VerificationRequestedCount from tblCase_additional_info where domainId = @DomainId and VerificationStatus = 'VER REQUESTED'
+END
