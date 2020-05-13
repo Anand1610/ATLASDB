98f7c66d6dff006ae5e1b53e29e3d0f91c846d13
tusharc2019 <51783385+tusharc2019@users.noreply.github.com>
2020-05-13 11:47:55 +0530

    editNames sp added

---
+++ dbo/Stored Procedures/Procs2/Update_InsInjPartyName.sql
@@ -0,0 +1,25 @@
+﻿CREATE Procedure [dbo].[Update_InsInjPartyName]
+@FirstName varchar(200),
+@LastName varchar(200),
+@CaseId varchar(50),
+@editValue varchar(50)
+
+AS
+BEGIN
+
+if @editValue ='updateInj'
+BEGIN
+ update tblcase set injuredparty_firstname =@FirstName  ,injuredparty_lastname=@LastName
+ where case_id= @CaseId
+END
+
+if @editValue ='updateIns'
+BEGIN
+update tblcase set insuredparty_firstname = @FirstName,insuredparty_lastname=@LastName
+where case_id=@CaseId
+END
+
+END
+GO
+
+
