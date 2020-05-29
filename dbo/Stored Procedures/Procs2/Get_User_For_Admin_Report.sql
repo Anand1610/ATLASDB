4a1be7dbb6b892b3ccb2dbfae8cde68f28187d86
Anand1610 <connectaidres1@greenyourbills.com>
2020-05-28 23:28:06 -0700

    webapp fix

---
+++ dbo/Stored Procedures/Procs2/Get_User_For_Admin_Report.sql
@@ -0,0 +1,13 @@
+﻿/* 
+modified by	Atul Jadhav
+Date		5/29/2020
+Description	Created for relove the web application error
+Last used by :
+*/
+CREATE PROCEDURE [dbo].[Get_User_For_Admin_Report]
+@DomainId varchar(50)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT UserName AS UserId,UserName from IssueTracker_Users(NOLOCK) where roleid<>2 order by UserName
+END
