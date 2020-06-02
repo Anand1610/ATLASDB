966fe620a219a3f8ed7e7e0db24310b5633dc332
Anand1610 <connectaidres1@greenyourbills.com>
2020-06-02 03:28:06 -0700

    Chnages for Status Disposition and abhay changes foe batch print

--- dbo/Stored Procedures/Procs2/Check_Case_Claim_Amount.sql
+++ dbo/Stored Procedures/Procs2/Check_Case_Claim_Amount.sql
@@ -1,14 +1,16 @@
-﻿CREATE PROCEDURE Check_Case_Claim_Amount
+﻿/*
+Updated By : abhay.w
+Updated Date : 06/02/2020
+*/
+CREATE PROCEDURE [dbo].[Check_Case_Claim_Amount]  -- Check_Case_Claim_Amount 'PDC20-101101,PDC20-101100,PDC20-100965','PDC','Complaint Packet-Over $8000-Non Conforming'
 (
 	@CaseId varchar(2000),
 	@DomainId varchar(10),
-	@BatchPrintType varchar(20)
+	@BatchPrintType varchar(100)
 )
 AS
 BEGIN
-
 	Declare @excludedCaseIds varchar(2000) = ''
-
 	Declare @totalCases INT = 0
 	DECLARE @CurrentCaseId varchar(25)
 	DECLARE @CurrentRowId INT = 0
@@ -18,7 +20,10 @@ BEGIN
 		BEGIN
 			SELECT @CurrentCaseId = ITEMS FROM [dbo].string_split(@CaseId,',') Where autoid = @CurrentRowId
 			Select @CLAIM_AMOUNT = Claim_Amount from tblCase where Case_Id = @CurrentCaseId	AND DomainId = @DomainId
-			IF((@CLAIM_AMOUNT > 8000 AND @BatchPrintType = 'STATEMENT OF CLAIMS') OR (@CLAIM_AMOUNT < 8000 AND @BatchPrintType = 'COMPLAINT PACKAGE'))
+			IF((@CLAIM_AMOUNT > 8000 AND @BatchPrintType = 'Complaint Packet-Under $8000-Non Conforming')
+				OR (@CLAIM_AMOUNT < 8000 AND @BatchPrintType = 'Complaint Packet-Over $8000-Conforming')
+				OR (@CLAIM_AMOUNT < 8000 AND @BatchPrintType = 'Complaint Packet-Over $8000-Non Conforming')
+				OR (@CLAIM_AMOUNT > 8000 AND @BatchPrintType = 'Complaint Packet-Under $8000-Conforming'))
 				BEGIN
 					IF(@excludedCaseIds != '')
 						BEGIN
